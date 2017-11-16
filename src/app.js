f322903f82d83123608ec96f4f754df05ae8559f
neurosoup <neurosoup@gmail.com>
2017-11-16 13:21:39 +0000

    updated to yakapa-common 1.0.5 and yakapa-agent-client 1.0.11

--- src/app.js
+++ src/app.js
@@ -2,25 +2,27 @@ import Agent from 'yakapa-agent-client'
 import * as Common from 'yakapa-common'
 import * as LZString from 'lz-string'
 
-const agent = new Agent({
-	port: 3002,
-	server: 'https://mprj.cloudapp.net',
-	tag: 'ea0d3ee6-ad65-47f4-9ff0-d25d7a18ed97',
+import express from 'express'
+
+const app = express().listen(3002)
+const client = new Agent.Client({
+	server: 'https://mprj.cloudapp.net', 
+	tag: 'ea0d3ee6-ad65-47f4-9ff0-d25d7a18ed97', 
 	nickname: 'Streaming'
 })
 
-agent.client.emitter.on('connected', () => {
-	Common.logger.info('Streaming connecté avec le tag', agent.client.tag)
+client.emitter.on('connected', () => {
+	Common.Logger.info('Streaming connecté avec le tag', client.tag)
 })
 
-agent.client.emitter.on('yakapa/stream', (socketMessage) => {
+client.emitter.on('yakapa/stream', (socketMessage) => {
 
 	const message = socketMessage.message
 	const from = socketMessage.from
 	const date = socketMessage.date
 	const decompressed = LZString.decompressFromUTF16(message)
 
-	Common.logger.info('Streaming', decompressed, 'asked from', from)
+	Common.Logger.info('Streaming', decompressed, 'asked from', from)
 	const jsonMessage = JSON.parse(decompressed)
 	
 })
