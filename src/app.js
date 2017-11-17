51825d628a8043d1a1def0750b28a7784c851d53
neurosoup <neurosoup@gmail.com>
2017-11-17 10:42:28 +0000

    streaming

--- src/app.js
+++ src/app.js
@@ -2,27 +2,67 @@ import Agent from 'yakapa-agent-client'
 import * as Common from 'yakapa-common'
 import * as LZString from 'lz-string'
 
+import * as path from 'path'
+import * as fs from 'fs'
+import dataForge from 'data-forge'
+
 import express from 'express'
 
+const SERVER = 'https://mprj.cloudapp.net'
+const NICKNAME = 'Streaming'
+const EMAIL = 'n/a'
+const TAG = 'ea0d3ee6-ad65-47f4-9ff0-d25d7a18ed97'
+const EVENT_PREFIX = 'yakapa'
+const STREAMED = `${EVENT_PREFIX}/streamed`
+
 const app = express().listen(3002)
-const client = new Agent.Client({
-	server: 'https://mprj.cloudapp.net', 
-	tag: 'ea0d3ee6-ad65-47f4-9ff0-d25d7a18ed97', 
-	nickname: 'Streaming'
-})
+const client = new Agent.Client({	server: SERVER,	tag: TAG,	nickname: NICKNAME })
+
+const concat = (tags, extractors, selectors) => {
+	let concatenatedData
+	tags.map(tag => {
+		const rootpath = path.join(__dirname, '..', '..', 'storage', tag)
+		extractors.map(extractor => {
+			const fullpath = path.join(rootpath, `${extractor}.json`)
+			if (fs.existsSync(fullpath)) {
+				const data = new dataForge.readFileSync(fullpath).parseJSON()
+				concatenatedData = concatenatedData ? concatenatedData.concat(data) : data
+			}
+		})
+	})
+	return concatenatedData
+}
+
+const applyQuery = (data, query) => {
+	const indexedData = data.parseDates("timestamp").setIndex("timestamp").orderBy(row => row.timestamp)
+	switch (query.name) {
+		case 'last':
+			return indexedData.last()
+		default:
+			return indexedData.last()
+	}
+}
+
+const applySelectors = (data, selectors) => {
+	const columns = data.getColumnNames()
+	const mandatorySelectors = selectors.concat(['timestamp', 'tag', 'extractor'])
+	const excludedColumns = columns.filter(column => mandatorySelectors.findIndex(column) === -1)
+	return data.dropSeries(excludedColumns)
+}
 
 client.emitter.on('connected', () => {
 	Common.Logger.info('Streaming connecté avec le tag', client.tag)
 })
 
 client.emitter.on('yakapa/stream', (socketMessage) => {
-
-	const message = socketMessage.message
-	const from = socketMessage.from
-	const date = socketMessage.date
+	const {	message, from, date } = socketMessage
 	const decompressed = LZString.decompressFromUTF16(message)
-
 	Common.Logger.info('Streaming', decompressed, 'asked from', from)
 	const jsonMessage = JSON.parse(decompressed)
-	
+	const {	name, tags,	extractors,	selectors, query } = jsonMessage
+	const concatenatedData = concat(tags, extractors, selectors)
+	const queryData = [].concat(applyQuery(concatenatedData, query))
+	const selectedData = applySelectors(new dataForge.DataFrame(queryData), selectors)			
+	const streamedMessage = {	name,	data: selectedData.toArray() }
+	client.emit(STREAMED, JSON.stringify(streamedMessage), from) 
 })
